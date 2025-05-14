4620c7e17243fecc4707aecb51a3015e92e0e3c9
l1zzart <lizaartemchenko@gmail.com>
2025-05-15 02:15:00 +0700

    Книги заготовка

---
+++ lab6/books.js
@@ -0,0 +1,90 @@
+const books = [
+    {
+        author: 'Евгений Замятин',
+        name: 'Мы',
+        publicationYear: 1920,
+        price: 550,
+        isBestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 700,
+        isBestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Сергей Довлатов',
+        name: 'Чемодан',
+        publicationYear: 1986,
+        price: 600,
+        isBestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 1833, 
+        price: 450,
+        isBestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 900,
+        isBestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Федор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 750,
+        isBestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Антон Чехов',
+        name: 'Вишневый сад',
+        publicationYear: 1904, 
+        price: 500,
+        isBestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Илья Ильф, Евгений Петров',
+        name: 'Двенадцать стульев',
+        publicationYear: 1928,
+        price: 650,
+        isBestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Максим Горький',
+        name: 'Мать',
+        publicationYear: 1906,
+        price: 550,
+        isBestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Мертвые души',
+        publicationYear: 1842,
+        price: 600,
+        isBestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Александр Грибоедов',
+        name: 'Горе от ума',
+        publicationYear: 1824,
+        price: 400,
+        isBestseller: false,
+        inStock: true
+    }
+];
